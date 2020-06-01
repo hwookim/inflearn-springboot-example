9770fa08d02aa55d001ed904a26bcfdae717b1d2
hwookim <gusdn9754@gmail.com>
2020-06-02 00:57:09 +0900

    feat: 외부 프로젝트에 @ConditionalOnMissingBean 추가 후 내부에서 같은 Bean 호출

--- src/main/java/inflearn/springboot/example/ExampleApplication.java
+++ src/main/java/inflearn/springboot/example/ExampleApplication.java
@@ -1,8 +1,10 @@
 package inflearn.springboot.example;
 
+import com.example.Example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApplication {
@@ -11,6 +13,14 @@ public class ExampleApplication {
         SpringApplication application = new SpringApplication(ExampleApplication.class);
         application.setWebApplicationType(WebApplicationType.NONE);
         application.run(args);
+    }
+
+    @Bean
+    public Example example() {
+        Example example = new Example();
+        example.setName("name");
+        example.setNumber(2L);
+        return example;
     }
 
 }
