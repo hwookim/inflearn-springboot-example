1baa591a9e4d48d9311c397d8e766c5dd7db6b19
hwookim <gusdn9754@gmail.com>
2020-06-05 17:53:42 +0900

    feat: UserControllerTest.createUser_JSON()

---
+++ src/main/java/inflearn/springboot/example/user/User.java
@@ -0,0 +1,20 @@
+package inflearn.springboot.example.user;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
